3f715858bc034f6e04a9f6393d3a79385cf59dc2
Pedro Bazilio <pedrohenriquebazilio8@gmail.com>
2026-02-20 10:19:52 -0300

    Update q5.java
    Updated verificarUnicos method to print a single message for unique or duplicate elements.

--- Atividade3_EstruturaDeDados/src/q5.java
+++ Atividade3_EstruturaDeDados/src/q5.java
@@ -1,37 +1,18 @@
 public class q5 {
-
-        public static boolean verificarUnicos(int[] vetor) {
+        public static void verificarUnicos(int vetor[]) {
             for (int i = 0; i < vetor.length; i++) {
                 for (int j = i + 1; j < vetor.length; j++) {
                     if (vetor[i] == vetor[j]) {
-                        System.out.println("Duplicata encontrada: O numero " + vetor[i] + " se repete.");
-                        return false; // Retorna false (não são únicos)
+                        System.out.println("Duplicata encontrada.\nA lista contém números repetidos.");
+                        return;
                     }
                 }
             }
-            return true;
+            System.out.println("Todos os elementos são únicos.");
         }
         public static void main(String[] args) {
-            // Exemplo 1: Lista com repetidos
             int[] lista1 = {10, 20, 30, 20, 50};
-
-            // Exemplo 2: Lista com todos únicos
-            int[] lista2 = {1, 2, 3, 4, 5};
-
-            System.out.println("--- Teste 1 ---");
-            if (verificarUnicos(lista1)) {
-                System.out.println("Todos os elementos são únicos.");
-            } else {
-                System.out.println("A lista contém números repetidos.");
-            }
-
-            System.out.println("\n--- Teste 2 ---");
-            if (verificarUnicos(lista2)) {
-                System.out.println("Todos os elementos são únicos.");
-            } else {
-                System.out.println("A lista contém números repetidos.");
-            }
+            verificarUnicos(lista1);
 
     }
 }
-
