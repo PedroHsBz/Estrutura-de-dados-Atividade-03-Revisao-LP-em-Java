a9ddeb7faf55764dd8fcf078c844339747e18d8a
Pedro Bazilio <pedrohenriquebazilio8@gmail.com>
2026-02-20 12:25:23 -0300

    Update q1.java

--- Atividade3_EstruturaDeDados/src/q1.java
+++ Atividade3_EstruturaDeDados/src/q1.java
@@ -1,23 +1,20 @@
-#include <stdio.h>
-#include <string.h>
-#define TAM 50 
-int main() {
-    char senha[TAM];
-    int c = 0, t = 0; 
-    scanf("%[^\n]%c", senha); 
-    for (int i = 0; i < strlen(senha); i++){
-        if (senha[i] == '@' || senha[i] == '#' || senha[i] == '$' || 
-            senha[i] == '%' || senha[i] == '&' || senha[i] == '*') {
-            c++;
-        } 
+public class q1 {
+    public static void main(String[] args){
+        double[] notas = {7.5, 4.0, 9.2, 5.5, 8.0, 6.5, 3.2, 10.0, 7.0, 5.8};
+        double soma = 0, media = 0, alunoAcima = 0, maiorN = 0;
+        for(int i = 0; i < notas.length; i++) {
+            soma += notas[i];
+            if (notas[i] > maiorN) {
+                maiorN = notas[i];
+            }
+        }
+        media=soma/notas.length;
+        for(int i = 0; i < notas.length; i++)
+            if (notas[i] >= media) {
+                alunoAcima++;
+            }
+        System.out.println("Media: "+media);
+        System.out.println("Alunos acima da media: "+alunoAcima);
+        System.out.println("Maior nota: "+maiorN);
     }
-    if (strlen(senha) < 8) {
-        printf("Invalida! A senha deve ter pelo menos 8 caracteres.\n");
-    } else if (c == 0) {
-        printf("Invalida! A senha deve conter pelo menos um caractere especial (@, #, $, etc).\n");
-    } else {
-        printf("Valida!\n");
-    }
-
-    return 0;
 }
