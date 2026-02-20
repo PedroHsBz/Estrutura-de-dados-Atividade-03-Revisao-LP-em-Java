0cb2bbf08e6fc80499c1e0a224b850fae113745c
Pedro Bazilio <pedrohenriquebazilio8@gmail.com>
2026-02-20 10:23:01 -0300

    Update q3.java

--- Atividade3_EstruturaDeDados/src/q3.java
+++ Atividade3_EstruturaDeDados/src/q3.java
@@ -3,18 +3,16 @@ public class q3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String senha = sc.nextLine();
-        int tamanho = senha.length();
-        for (int i = 0; i < tamanho; i++) {
-            char c = senha.charAt(i);
-        }
-        if (tamanho < 8) {
-            System.out.println("Inválida!\n A senha deve possuir pelo menos 8 caracteres.");
+        while (senha.length() < 8) {
+            System.out.println("Inválida!\nA senha deve possuir pelo menos 8 caracteres.");
+            System.out.println("Tente novamente: ");
+            senha = sc.nextLine();
         }
         if(senha.contains("@") || senha.contains("#") || senha.contains("$")|| senha.contains("%")|| senha.contains("&")){
-            System.out.println("Inválida!\n A senha deve conter pelo menos um caractere especial (@, #, $, %, & ou *).");
+            System.out.println("Válida!");
         }
         else {
-            System.out.println("Válida!");
+            System.out.println("Inválida!\nA senha deve conter pelo menos um caractere especial (@, #, $, %, & ou *).");
         }
     }
 }
